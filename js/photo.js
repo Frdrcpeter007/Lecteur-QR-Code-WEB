7300d7e050a844308647cac77e1e3cf324acd5f0
frdrcpeter007 <frdrcpeter@gmail.com>
2020-05-30 14:27:53 +0100

    Ajout d'un bouton de basculement de selfie à arrière

--- js/photo.js
+++ js/photo.js
@@ -3,31 +3,27 @@
         context = canvas.getContext("2d"),
         video = document.querySelector('#video'),
         img = document.querySelector("#myImage");
+
+        var front = false;
+        document.getElementById('flip-button').onclick = function() { front = !front; };
+
+        //add constraints object
+        var constraints = { audio: false,
+                            video: { facingMode: (front? "user" : "environment") } };
     
-    
-    navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "user" } })
-    .then(function (stream) {
-        video.srcObject = stream;
-        video.onloadedmetadata = function (e) {
+        navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream){
+            video.srcObject = mediaStream;
             video.play();
-        };
-    })
-    .catch(function (err) {
-        alert("Aucune autorisation accordé : " + err);
-    });
+        }).catch(function(err){
+            console.log("yikes, and err!" + err.message);
+        });
 
-    document.getElementById("capture").addEventListener("click", (e) => {
-        e.preventDefault();
-
-        context.drawImage(video, 0, 0, 400, 300);
-        window.localStorage.setItem("aaa", canvas.toDataURL('image/png'));
-        img.setAttribute('src', canvas.toDataURL('image/png'))
-    })
-
-    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-        alert("Let's get this party started")
-      }else {
-          alert("ouff")
-      }
+        document.getElementById("capture").addEventListener("click", (e) => {
+            e.preventDefault();
+        
+            context.drawImage(video, 0, 0, 400, 300);
+            window.localStorage.setItem("aaa", canvas.toDataURL('image/png'));
+            img.setAttribute('src', canvas.toDataURL('image/png'))
+        })
 
 })();
